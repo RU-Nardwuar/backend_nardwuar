8182c8d114b8b6977cc596686b32538244c0eb00
James Wo <jameswo1234@gmail.com>
2019-03-15 17:08:40 -0400

    edited regsitration

--- app.py
+++ app.py
@@ -2,19 +2,20 @@ from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials
 from pymongo import MongoClient
-client = MongoClient()
-cred = credentials.Certificate('/Users/jameswo/Documents/Nardwuar/nardwuar-958fc-firebase-adminsdk-4euwu-f815d4afc9.json')
+from werkzeug.wrappers import Request, Response
+# client = MongoClient()
+cred = credentials.Certificate('nardwuar-958fc-firebase-adminsdk-4euwu-f815d4afc9.json')
 default_app = firebase_admin.initialize_app(cred)
 app=Flask(__name__)
 
 #database for users
-users_db = client['users-db']
+# users_db = client['users-db']
 
 #route for user registration
 @app.route("/register", methods = ["POST"])
 def users():
      #need id_token from frontend
-     id_token = request.get_json()["id token"]
+     id_token = request.get_json()["id_token"]
      #verify_id_token() verifies signature and data for provided JWT. Returns a dictionary of key-value pairs parsed from the decoded JWTself.
      #raises ValueError if jwt was found t be invalid,
      #raises AuthError if check_revoked is requested and token was revoked
@@ -27,7 +28,7 @@ def users():
          "Username": request.get_json()["Username"],
          "FollowedArtists": []
          }
-         users_db.insert_one(newUser)
+         # users_db.insert_one(newUser)
          response ={
          "status":"Success",
          "message":"Account created!"
@@ -35,15 +36,28 @@ def users():
          return jsonify(data);
 
      except ValueError:
-         raise LoginError("JWT was found to be invalid")
+         return jsonify({"error": "Bad Token"})
      except AuthError:
-         raise LoginError("Token was revoked")
-
-@app.route("/UserInfoLogin", methods = ["GET"])
-def getInfo():
-    
-
-class LoginError(Exception):
-     code = 401
-     def __init__(self, message):
-         self.message = message
+         return jsonify({"error:" "Token was revoked"})
+#
+# @app.route("/artists", methods = ["GET"])
+# @auth_required
+# def getInfoForLogin():
+#     return jsonify()
+#
+#
+# class LoginError(werkzeug.exceptions.HTTPException):
+#      code = 401
+#      def __init__(self, message):
+#          self.message = message
+#
+# app.register_error_handler(LoginError, handle_401)
+#
+# def auth_required(f):
+#    @wraps(f)
+#     def verify_token(*args, **kwargs):
+#         try:
+#             id_token = request.get_json()["id_token"]
+#             decoded_token = auth.verify_id_token(id_token)
+#         except:
+#             response
