751177595a4b49ca845ce6e1acc60a9de4d0710a
James Wo <jameswo1234@gmail.com>
2019-04-22 22:26:39 -0400

    e

--- app.py
+++ app.py
@@ -129,9 +129,11 @@ def searchArtistInfo(artist_id):
     for item in list_of_albums:
         list_of_albums_names.append(item['name'])
 
+
     seen = set()
     seen_add=seen.add
-    list_of_albums_names_no_duplicates = [x for x in list_of_albums_names if not (x in seen or seen_add(x))]
+    list_of_albums_names_no_duplicates = list_of_albums_names
+    #list_of_albums_names_no_duplicates = [x for x in list_of_albums_names if not (x in seen or seen_add(x))]
     albumList=[]
     artistInfo = {
         "Spotify":{
